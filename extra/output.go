bfe9ebdaa4ade855d706b5fd41dfc5db12e19528
ZeinDeRikko <129055694+ZeinDeRikko@users.noreply.github.com>
2024-02-23 15:11:50 +0200

    Update output.go

--- extra/output.go
+++ extra/output.go
@@ -14,7 +14,7 @@ func ShowLogo() {
 func ShowDevInfo() {
 	fmt.Println("\033[36mVERSION: \033[33m1.0\033[33m")
 	fmt.Println("\033[36mDEV: \033[33mhttps://t.me/StarLabsTech\033[33m")
-	fmt.Println("\033[36mGitHub: \033[33mhttps://github.com/0xStarLabs/StarLabs-Twitter\033[33m")
+	fmt.Println("\033[36mGitHub: \033[33mhttps://github.com/0xStarLabs/StarLabs-Galxe-KYC-Grabber\033[33m")
 	fmt.Println("\033[36mDONATION EVM ADDRESS: \033[33m0x620ea8b01607efdf3c74994391f86523acf6f9e1\033[0m")
 	fmt.Println()
 }
